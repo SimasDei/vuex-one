78cd084e2d3bc049a8630e204ecd9d6f1a65c5c2
Simas <simonas@digitouch.lt>
2019-04-09 17:35:58 +0300

    add Vuex store, setup Central State

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,10 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Vuex</h1>
-                <app-result :counter="counter"></app-result>
+                <app-result></app-result>
+                <app-another-result></app-another-result>
                 <hr>
-                <app-counter @updated="counter += $event"></app-counter>
+                <app-counter></app-counter>
             </div>
         </div>
     </div>
@@ -14,16 +15,13 @@
 <script>
     import Counter from './components/Counter.vue';
     import Result from './components/Result.vue';
+    import AnotherResult from './components/AnotherResult.vue';
 
     export default {
-        data() {
-            return {
-                counter: 0
-            }
-        },
         components: {
             appCounter: Counter,
             appResult: Result,
+            appAnotherResult: AnotherResult
         }
     }
 </script>
