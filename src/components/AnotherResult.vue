fb6dfe4ceae49acb79b331630b99982dc5d68c90
Simas <simonas@digitouch.lt>
2019-04-09 17:56:54 +0300

    add Babel preset for spread operator, use map getters to combine

--- src/components/AnotherResult.vue
+++ src/components/AnotherResult.vue
@@ -1,13 +1,17 @@
 <template>
-    <p>Counter is: {{ counter }}</p>
+<div>
+    <p>Counter is: {{ doubleCounter }}</p>
+    <p>Number of Clicks: {{stringCounter}}</p>
+</div>
 </template>
 
 <script>
+import {mapGetters} from 'vuex';
     export default {
        computed: {
-           counter() {
-               return this.$store.state.counter * 2;
-           }
-       }
+           ...mapGetters([
+            'doubleCounter',
+            'stringCounter'
+       ])}
     }
 </script>
